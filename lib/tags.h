15bdaa1fdd0a17a69edf0b200f947c4b4ab06406
fer-hnndz <jfernandohernandez28@gmail.com>
2025-02-25 12:22:44 -0600

    Added CLI parsing

---
+++ lib/tags.h
@@ -0,0 +1,6 @@
+#define HELP_TAG "--help"
+#define HELP_MESSAGE "A CLI that evaluates mathematical expressions.\
+\nUsage: operate <expression> [options]\
+\n\nOptions:\
+\n--help - Shows this message \
+\n--debug - Prints debug messages to the terminal included with the evaluated expression."
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <string>
 
-std::string parseInputs(int argc, char *argv[]);
+#include <tags.h>
+
+std::string parseInputs(int argc, char *argv[], bool &debug);
 
 int main(int argc, char *argv[])
 {
 
-    parseInputs(argc, argv);
+    bool debug = false;
+    const std::string expr = parseInputs(argc, argv, debug);
+
+    std::cout << "Parsed Expression: " << expr << "\n";
+    std::cout << "Run in debug mode: " << debug << "\n";
 
     return 0;
 }
@@ -15,6 +21,29 @@ int main(int argc, char *argv[])
  * Parses the CLI inputs and returns the expression the user inputted.
  * Prints the help menu if --help tag is specified.
  */
-std::string parseInputs(int argc, char *argvp[])
+std::string parseInputs(int argc, char *argv[], bool &debug)
 {
+
+    // Check if user has enough arguments
+    if (argc < 2)
+    {
+        std::cout << HELP_MESSAGE << "\n";
+        return "";
+    }
+
+    const std::string expr = argv[1];
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (std::string(argv[i]) == HELP_TAG)
+        {
+            std::cout << HELP_MESSAGE;
+            return "";
+        }
+
+        if (std::string(argv[i]) == "--debug")
+            debug = true;
+    }
+
+    return expr;
 }
