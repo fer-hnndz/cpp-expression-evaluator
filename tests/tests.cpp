b996fb7bcc49cc88d76af2f0520be05f0f084b70
fer-hnndz <jfernandohernandez28@gmail.com>
2025-03-12 10:55:32 -0600

    Solve all success tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -10,7 +10,7 @@ TEST_CASE("Successful Expressions are computed", "[evaluate-success]") {
   REQUIRE(ev.execute("10/2+7", false) == 12);
   REQUIRE(ev.execute("(2+3)*4", false) == 20);
   REQUIRE(ev.execute("(8/(4-2))+6", false) == 10);
-  REQUIRE(ev.execute("((((((4 + 2) * (5 - 3)) + (6 / (2 + 1))) - (9 % (4 + 3))) * (7 + 1)) / 2", false) == 48);
+  REQUIRE(ev.execute("(((((4 + 2) * (5 - 3)) + (6 / (2 + 1))) - (9 % (4 + 3))) * (7 + 1)) / 2", false) == 48);
   REQUIRE(ev.execute("3+2^3*2", false) == 19);
   REQUIRE(ev.execute("((3+5)*2)-((4-1)*3)", false) == 7);
   REQUIRE(ev.execute("(3+2)^(2+1)", false) == 125);
